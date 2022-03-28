a82f90550b1d7ed3d3df2a584a62ee4c77b7fb60
twd10 <tzuoyeah@gmail.com>
2022-03-29 02:27:49 +0800

    viewmenu微調

--- src/components/ViewMenu.js
+++ src/components/ViewMenu.js
@@ -4,10 +4,11 @@ import Fall from './ViewMenu/Fall'
 import pastes from '../data/pastes.json'
 
 export default function ViewMenu(){
-    return(
-          <article className='row row-cols-1'>
-            <div className='col bg-white border-bottom sticky-top py-2'>閱覽模式</div>
-            <Fall pastes={pastes}/>
-          </article>
-    )
+  const modeName = ["貼文模式","",""]
+  return(
+        <article className='row row-cols-1'>
+          <div className='col bg-white border-bottom sticky-top py-2 fs-5'>貼文模式</div>
+          <Fall pastes={pastes}/>
+        </article>
+  )
 }
